ebb5cfa6e6d4c16ae7b98ddf8893ac3d6c5bf89d
Neo Ludolph <neocan20@gmail.com>
2026-09-14 23:07:38 +0200

    add unit tests for TaskModel constructor and attributes validation

---
+++ src/test/java/de/neoludolph/task_tracker_cli/TaskModelTest.java
@@ -0,0 +1,20 @@
+package de.neoludolph.task_tracker_cli;
+
+import de.neoludolph.task_tracker_cli.Model.TaskModel;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TaskModelTest {
+    @Test
+    void constructorSetsAttributes() {
+        TaskModel taskModel = new TaskModel("Shopping");
+
+        assertEquals("Shopping", taskModel.getDescription());
+        assertEquals(TaskModel.Status.TODO, taskModel.getStatus());
+        assertTrue(taskModel.getCreatedAt().isBefore(LocalDateTime.now()));
+    }
+}
