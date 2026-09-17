20b584f6fa2e24e08acafc82a9cd1da13166ded3
Neo Ludolph <neoludolph@gmail.com>
2026-09-17 15:43:46 +0200

    add unit test for TaskServiceImpl add method using Mockito

--- src/test/java/de/neoludolph/task_tracker_cli/Service/TaskServiceImplTest.java
+++ src/test/java/de/neoludolph/task_tracker_cli/Service/TaskServiceImplTest.java
@@ -1,4 +1,22 @@
 package de.neoludolph.task_tracker_cli.Service;
 
+import de.neoludolph.task_tracker_cli.Model.TaskModel;
+import de.neoludolph.task_tracker_cli.Repository.TaskRepositoryImpl;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
 public class TaskServiceImplTest {
+    @Test
+    void testTaskServiceAdd() throws IOException {
+        String description = "Shopping";
+        TaskRepositoryImpl taskRepository = mock(TaskRepositoryImpl.class);
+        TaskServiceImpl taskService = new TaskServiceImpl(taskRepository);
+        taskService.add(description);
+        verify(taskRepository).saveNewTaskInJson(any(TaskModel.class));
+    }
 }
