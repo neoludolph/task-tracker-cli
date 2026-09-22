f4481d88f2f44fc389a1de114b43065d776ec608
Neo Ludolph <neoludolph@gmail.com>
2026-09-22 11:33:24 +0200

    add unit tests for TaskServiceImpl methods using Mockito

--- src/test/java/de/neoludolph/task_tracker_cli/Service/TaskServiceImplTest.java
+++ src/test/java/de/neoludolph/task_tracker_cli/Service/TaskServiceImplTest.java
@@ -3,9 +3,11 @@ package de.neoludolph.task_tracker_cli.Service;
 import de.neoludolph.task_tracker_cli.Model.TaskModel;
 import de.neoludolph.task_tracker_cli.Repository.TaskRepositoryImpl;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
 import java.io.IOException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -17,6 +19,94 @@ public class TaskServiceImplTest {
         TaskRepositoryImpl taskRepository = mock(TaskRepositoryImpl.class);
         TaskServiceImpl taskService = new TaskServiceImpl(taskRepository);
         taskService.add(description);
-        verify(taskRepository).saveNewTaskInJson(any(TaskModel.class));
+
+        ArgumentCaptor<TaskModel> captor = ArgumentCaptor.forClass(TaskModel.class);
+
+        verify(taskRepository).saveNewTaskInJson(captor.capture());
+
+        TaskModel savedTask = captor.getValue();
+
+        assertEquals(description, savedTask.getDescription());
+    }
+
+    @Test
+    void testTaskServiceUpdate() throws IOException {
+        long id = 1;
+        String description = "Shopping";
+        TaskRepositoryImpl taskRepository = mock(TaskRepositoryImpl.class);
+        TaskServiceImpl taskService = new TaskServiceImpl(taskRepository);
+        taskService.update(id, description);
+
+        verify(taskRepository).saveUpdatedTaskInJson(id, description);
+    }
+
+    @Test
+    void testTaskServiceDelete() throws IOException {
+        long id = 1;
+        TaskRepositoryImpl taskRepository = mock(TaskRepositoryImpl.class);
+        TaskServiceImpl taskService = new TaskServiceImpl(taskRepository);
+        taskService.delete(id);
+
+        verify(taskRepository).deleteTaskInJson(id);
+    }
+
+    @Test
+    void testTaskServiceMarkInProgress() throws IOException {
+        long id = 1;
+        TaskRepositoryImpl taskRepository = mock(TaskRepositoryImpl.class);
+        TaskServiceImpl taskService = new TaskServiceImpl(taskRepository);
+        taskService.markInProgress(id);
+
+        verify(taskRepository).markTaskAsInProgress(id);
+    }
+
+    @Test
+    void testTaskServiceMarkDone() throws IOException {
+        long id = 1;
+        TaskRepositoryImpl taskRepository = mock(TaskRepositoryImpl.class);
+        TaskServiceImpl taskService = new TaskServiceImpl(taskRepository);
+        taskService.markDone(id);
+
+        verify(taskRepository).markTaskAsDone(id);
+    }
+
+    @Test
+    void testTaskServiceListAll() throws IOException {
+        long id = 1;
+        TaskRepositoryImpl taskRepository = mock(TaskRepositoryImpl.class);
+        TaskServiceImpl taskService = new TaskServiceImpl(taskRepository);
+        taskService.listAll();
+
+        verify(taskRepository).listAllTasks();
+    }
+
+    @Test
+    void testTaskServiceListDone() throws IOException {
+        long id = 1;
+        TaskRepositoryImpl taskRepository = mock(TaskRepositoryImpl.class);
+        TaskServiceImpl taskService = new TaskServiceImpl(taskRepository);
+        taskService.listDone();
+
+        verify(taskRepository).listDoneTasks();
+    }
+
+    @Test
+    void testTaskServiceListToDo() throws IOException {
+        long id = 1;
+        TaskRepositoryImpl taskRepository = mock(TaskRepositoryImpl.class);
+        TaskServiceImpl taskService = new TaskServiceImpl(taskRepository);
+        taskService.listToDo();
+
+        verify(taskRepository).listToDoTasks();
+    }
+
+    @Test
+    void testTaskServiceListInProgress() throws IOException {
+        long id = 1;
+        TaskRepositoryImpl taskRepository = mock(TaskRepositoryImpl.class);
+        TaskServiceImpl taskService = new TaskServiceImpl(taskRepository);
+        taskService.listInProgress();
+
+        verify(taskRepository).listInProgressTasks();
     }
 }
